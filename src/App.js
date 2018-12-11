4a70b128319f449a367d08ce00adc8cbf035f15a
Nabil Fannane <mzabi30@gmail.com>
2018-12-11 14:50:23 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Navbar from './Components/Navbar/Navbar'
+import List from './Components/List/List'
 
 
 class App extends Component {
   render() {
-    return  <Navbar />
+    return (
+      <React.Fragment>
+      <Navbar />
+      <List />
+
+    </React.Fragment>
+    )
+  
+   
   
   }
 }
---
+++ src/Components/List/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './List.scss'
+
+export default class List extends Component {
+  render() {
+    return (
+      <section>
+   s;ldksd;lfksd;lk
+          
+     </section>
+    )
+  }
+}
