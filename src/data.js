65110300872bba048fbbfc2fed0165af0dad5af4
Nabil Fannane <mzabi30@gmail.com>
2018-12-11 13:38:46 -0800

    tour

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const data=[{
+
+    id:1,
+    city:'New york',
+    img:'https://images.pexels.com/photos/466685/pexels-photo-466685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name:'New York Tour',
+    info:'skjvfhskfhsjfkhsfjkh'
+},
+{
+
+    id:2,
+    city:'Boston',
+    img:'https://images.pexels.com/photos/416998/pexels-photo-416998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name:'New York Tour',
+    info:'skjvfhskfhsjfkhsfjkh'
+},
+{
+
+    id:3,
+    city:'Paris',
+    img:'https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    name:'New York Tour',
+    info:'skjvfhskfhsjfkhsfjkh'
+},
+{
+
+    id:4,
+    city:'Casablanca',
+    img:'https://images.pexels.com/photos/943533/pexels-photo-943533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name:'New York Tour',
+    info:'skjvfhskfhsjfkhsfjkh'
+}
+]
