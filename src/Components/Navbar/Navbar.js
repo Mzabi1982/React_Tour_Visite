1b639d2570cd8810edc729159412c61a8b49072b
Nabil Fannane <mzabi30@gmail.com>
2018-12-12 09:44:54 -0800

    updates

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar=()=> {
   
   return (
    <nav className="navbar">
-<img width="110px" height="110px"src='https://www.visitwinchester.co.uk/app/uploads/2018/05/iroundel.jpg'/>
+<img width="110px" height="110px"src='https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1478555/580/386/m1/fpnw/wm0/travel-agency-logo-.jpg?1491942632&s=c9fb528107a59e68730ed05719bd8036'/>
    <ul>
      <li>
        <a href="/" className="alink">
